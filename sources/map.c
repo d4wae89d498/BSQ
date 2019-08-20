b215de16091b305d1955c3e95ee99b6fbd83a94f
Marc Faussurier <mfaussur@z1r7p6.le-101.fr>
2019-08-20 19:04:21 +0200

    guele fonction

---
+++ sources/map.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   square.c                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mfaussur <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/08/20 18:37:14 by mfaussur     #+#   ##    ##    #+#       */
+/*   Updated: 2019/08/20 19:03:53 by mfaussur    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "./../includes/map.h"
+#include "./../includes/square.h"
+
+t_map		ft_fill_map(char *map_buffer)
+{
+	t_map		map;
+
+	(void)map;
+	(void)map_buffer;
+	return (map);
+}
+
+t_square	*ft_find_biggest_square(t_map *map)
+{
+	t_square	*biggest_square;		
+
+	(void)biggest_square;
+	(void)map;
+	return (0);
+}
+
+void		ft_display_map(t_map *map, t_square *biggest_square)
+{
+	(void)map;
+	(void)biggest_square;
+}
