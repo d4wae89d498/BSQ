8c7d8fb49aaea70524ab5de85a7af2955c695667
Francois-xavier Faivre <ffaivre@z2r1p2.le-101.fr>
2019-08-20 20:12:49 +0200

    je n en peux plus

--- sources/square.c
+++ sources/square.c
@@ -3,20 +3,23 @@
 /*                                                              /             */
 /*   square.c                                         .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: mfaussur <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*   By: ffaivre <ffaivre@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/20 18:55:26 by mfaussur     #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/20 19:02:55 by mfaussur    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/20 20:12:02 by ffaivre     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "./../includes/square.h"
+#include <stdlib.h>
 
 t_square	*ft_create_square(int x, int y, int l)
 {
-	(void)x;
-	(void)y;
-	(void)l;
-	return (0);
+	t_square *pointeur;
+	pointeur = malloc(sizeof(t_square));
+	pointeur ->x = x;
+	pointeur ->y = y;
+	pointeur ->l = l;
+	return (pointeur);
 }
